9fb834fa3ba2f3f430bcc69a5f0be0f90bfce0c4
Kaushalyasl <149307067+Kaushalyasl@users.noreply.github.com>
2025-01-15 00:00:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=C3wCCKyI#fa0Wu3hszCr2HGfXTJ4PGRUaQ0gcQVXQ4EHlhmU1lS8' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=LqYUHa6b#xYAHLdv3fF3CRXABoOZalqbGE_WinXyL8ae0oVzApoo' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
